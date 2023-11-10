98eeded7c06e455b9f894daff9eb262e3c7b6e38
Damian Esene <damianesene@Damians-MacBook-Air.local>
2023-11-10 16:50:13 -0500

    create k8s roles and eks

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -69,6 +69,16 @@ resource "aws_eks_cluster" "eks_cluster"{
     }
 }
 
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+ #create OpenID connect provider
+resource "aws_iam_openid_connect_provider" "eks"{
+    client_id_list = ["sts.amazonaws.com"]
+    thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+    url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+}
+
 #Create Node Group
 resource "aws_eks_node_group" "private_nodes"{
     cluster_name = aws_eks_cluster.eks_cluster.name
@@ -80,10 +90,69 @@ resource "aws_eks_node_group" "private_nodes"{
 
     ]
     capacity_type = "ON_DEMAND"
-    instance_types = ["t2.small"]
+    instance_types = ["t3.small"]
     scaling_config   {
         desired_size = 1
-        max_size    = 1
-        min_size    =1
+        max_size    = 3
+        min_size    =0
     }
+
+    update_config {
+      max_unavailable = 1
+    }
+
+    labels = {
+        role = "general"
+    }
+}
+
+
+#test provider by creating a role
+data "aws_iam_policy_document" "test_oidc"{
+    statement {
+        actions =   ["sts:AssumeRoleWithWebIdentity"]
+        effect  = "Allow"
+
+        condition {
+            test = "StringEquals"
+            variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://","")}:sub"
+            values = ["system:serviceaccount:default:aws-test"]
+        }
+
+        principals {
+            identifiers = [aws_iam_openid_connect_provider.eks.arn]
+            type    = "Federated"
+        }
+    }
+}
+
+resource "aws_iam_role" "test_oidc"{
+    assume_role_policy = data.aws_iam_policy_document.test_oidc.json
+    name = "test_oidc"
+}
+
+resource "aws_iam_policy" "test_policy"{
+    name = "test_policy"
+
+    policy = jsonencode({
+        Statement = [{
+            Action = [
+                "s3:ListAllMyBuckets",
+                "s3:GetBucketLocation"
+            ]
+            Effect = "Allow"
+            Resource = "arn:aws:s3:::*"
+
+        }]
+        Version = "2012-10-17"
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "test_attach"{
+    role = aws_iam_role.test_oidc.name
+    policy_arn = aws_iam_policy.test_policy.arn
+}
+
+output "test_policy_arn"{
+    value = aws_iam_role.test_oidc.arn
 }
